72b997d480bc8909f3632fb50a45df858d541911
Exsper <starlightexsper@foxmail.com>
2024-02-07 19:51:37 +0800

    1.2.0

--- YNO 2kki var44 Guess Helper.js
+++ YNO 2kki var44 Guess Helper.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name         YNOproject Yume2kki 变量44 推测
 // @namespace    https://github.com/Exsper/
-// @version      1.1.3
-// @description  本工具通过从HEAPU8中检索入睡次数（变量#43）来推测变量#44的地址
+// @version      1.2.0
+// @description  本工具通过从HEAPU32中检索入睡次数（变量#43）来推测变量#44的地址
 // @author       Exsper
 // @homepage     https://github.com/Exsper/yno2kkivar44guess#readme
 // @supportURL   https://github.com/Exsper/yno2kkivar44guess/issues
 // @match        https://ynoproject.net/2kki/
 // @require      https://cdn.staticfile.org/jquery/2.1.3/jquery.min.js
-// @grant        none
+// @license      MIT License
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @run-at       document-end
 // ==/UserScript==
 
@@ -16,9 +18,10 @@
 
 /**
  * @param {Array<number>} sourceArray 
- * @param {Array<number>} aimArray 
+ * @param {Array<number>|number} aimArray 
  */
 function findDataIndex(sourceArray, aimArray) {
+    if (typeof aimArray == "number") aimArray = [aimArray];
     let index = 0;
     let MAX_Index = sourceArray.length - 1;
     let cmpIndex = 0;
@@ -41,36 +44,18 @@ function findDataIndex(sourceArray, aimArray) {
     return finds;
 }
 
-function Int2HEAPU8Array(num) {
-    let s = num.toString(16);
-    let ol = s.length;
-    for (let i = 0; i < 8 - ol; i++) {
-        s = "0" + s;
-    }
-    let s1 = parseInt("0x" + s.substring(6, 8));
-    let s2 = parseInt("0x" + s.substring(4, 6));
-    let s3 = parseInt("0x" + s.substring(2, 4));
-    let s4 = parseInt("0x" + s.substring(0, 2));
-    return [s1, s2, s3, s4];
-}
-
-/**
- * @param {Array<number>} array
- */
-function HEAPU8Array2Int(array) {
-    return array.reduce((sum, cur, index) => sum += cur * Math.pow(256, index));
-}
-
-function getSeasonString(seasonNumber) {
+function getSeasonString(sleepCount, seasonNumber) {
+    if (seasonNumber !== getSeason(sleepCount)) return "错误";
+    let dayIndex = (sleepCount + 1) % 3 + 1;
     switch (seasonNumber) {
         case 0:
-            return '冬';
+            return '冬' + " 第" + dayIndex + "天";
         case 1:
-            return '春';
+            return '春' + " 第" + dayIndex + "天";
         case 2:
-            return '夏';
+            return '夏' + " 第" + dayIndex + "天";
         case 3:
-            return '秋';
+            return '秋' + " 第" + dayIndex + "天";
         default:
             return '错误';
     }
@@ -84,15 +69,14 @@ function getSeason(sleepTimes) {
 function filterMemory(sleepIndexList, sleepCount) {
     let qualifiedList = [];
     for (let i = 0; i < sleepIndexList.length; i++) {
-        let sleepCountFirst = sleepIndexList[i]; // 入睡次数第一个字节，属于#43
-        if (sleepCountFirst % 4 != 0) continue; // 4字节存储
-        let num44Index = sleepCountFirst + 4;
-        // #44只有0-255，后三个字节均为0
-        if (easyrpgPlayer["HEAPU8"][num44Index + 1] != 0 || easyrpgPlayer["HEAPU8"][num44Index + 2] != 0 || easyrpgPlayer["HEAPU8"][num44Index + 3] != 0) continue;
+        let num44Index = sleepIndexList[i] + 1;
+        // #44范围为0-255
+        if (easyrpgPlayer["HEAPU32"][num44Index] > 255) continue;
         // #94为季节，检查#94的值
-        if (easyrpgPlayer["HEAPU8"][num44Index + 200] != getSeason(sleepCount)) continue;
+        if (easyrpgPlayer["HEAPU32"][num44Index + 50] != getSeason(sleepCount)) continue;
         // 以下为可能值
-        if (easyrpgPlayer["HEAPU8"][num44Index] != 0) // #44很可能不为0，将不为0的位置前置
+        if (easyrpgPlayer["HEAPU32"][num44Index] != 0)
+            // #44很可能不为0，将不为0的位置前置
             qualifiedList.unshift(num44Index);
         else
             qualifiedList.push(num44Index);
@@ -100,14 +84,37 @@ function filterMemory(sleepIndexList, sleepCount) {
     return qualifiedList;
 }
 
+class CustomVariable {
+    constructor(index, name) {
+        this.index = index;
+        this.name = name;
+    }
+
+    toTableData() {
+        return { title: this.name, index: this.index, callFuc: null };
+    }
+}
+
 class Script {
     constructor() {
         this.memoryIndexs = [];
         this.correctNum44Index = -1;
+        this.customVars = [];
+    }
+
+    loadStorage() {
+        let customVarsData = GM_getValue("customVarsData", []);
+        this.customVars.push(...(customVarsData.map((data) => new CustomVariable(data[0], data[1]))));
+    }
+
+    saveStorage() {
+        let customVarsData = this.customVars.map((customVar) => [customVar.index, customVar.name]);
+        GM_setValue("customVarsData", customVarsData);
     }
 
     init() {
-        let $openButton = $('<button>', { text: "+", id: "rs-open", style: "float:left;top:30%;position:absolute;left:0%;" }).appendTo($("body"));
+        this.loadStorage();
+        let $openButton = $('<button>', { text: "+", id: "rs-open", style: "float:left;top:30%;position:absolute;left:0%;", title: "显示窗口" }).appendTo($("body"));
         $openButton.click(() => {
             $("#rs-div").show();
             $("#rs-open").hide();
@@ -126,7 +133,7 @@ class Script {
                 $statLabel.text("请输入正整数");
                 $checkButton.text("确定");
             }
-            let result = filterMemory(findDataIndex(easyrpgPlayer["HEAPU8"], Int2HEAPU8Array(sleepCount)), sleepCount);
+            let result = filterMemory(findDataIndex(easyrpgPlayer["HEAPU32"], sleepCount), sleepCount);
             if (result.length <= 0) {
                 $checkButton.attr("disabled", false);
                 $statLabel.text("找不到符合条件的地址，请重试");
@@ -134,7 +141,7 @@ class Script {
             }
             else {
                 this.memoryIndexs = result;
-                this.isZero = this.memoryIndexs.map(mi => easyrpgPlayer["HEAPU8"][mi] == 0);
+                this.isZero = this.memoryIndexs.map(mi => easyrpgPlayer["HEAPU32"][mi] == 0);
                 if (this.memoryIndexs.length > 1) {
                     $statLabel.text("找到多个符合条件的地址，请选择正确的#44变量。建议到伪部屋睡几次床，每次都会更新的就是#44变量");
                     this.updateSelectTable();
@@ -147,7 +154,27 @@ class Script {
                 $checkButton.hide();
             }
         });
-        let $closeButton = $('<button>', { text: "-", id: "rs-close", style: "align-self: end;" }).prependTo($mainDiv);
+        let $titleTable = $("<table>", { id: "rs-title", style: "table-layout:fixed;" }).prependTo($mainDiv);
+        let $thead = $("<thead>").appendTo($titleTable);
+        let $tr = $("<tr>").appendTo($thead);
+        let $td = $("<td>").appendTo($tr);
+        let $addVarButton = $('<button>', { text: "+", id: "rs-addvar", style: "float: left;", title: "添加自定义变量" }).appendTo($td);
+        $addVarButton.hide();
+        $addVarButton.click(() => {
+            let index;
+            let name;
+            while (!index || typeof index !== "number" || index < 0) {
+                index = parseInt(prompt("请输入变量ID，例如：3513"));
+            }
+            while (!name) {
+                name = prompt("请给变量起个名字，例如：踏破率");
+            }
+            this.customVars.push(new CustomVariable(index, name));
+            alert("添加成功！如需删除请点击变量名。");
+            this.saveStorage();
+        });
+        $td = $("<td>").appendTo($tr);
+        let $closeButton = $('<button>', { text: "-", id: "rs-close", style: "float: right;", title: "隐藏窗口" }).appendTo($td);
         $closeButton.click(() => {
             $("#rs-div").hide();
             $("#rs-open").show();
@@ -158,6 +185,7 @@ class Script {
 
     updateSelectTable() {
         if (this.memoryIndexs.length <= 0) return;
+        this.memoryIndexs = this.memoryIndexs.filter((mi) => easyrpgPlayer["HEAPU32"][mi] <= 255);
         let $mainTable = $("#rs-table");
         $mainTable.empty();
         let $thead = $("<thead>").appendTo($mainTable);
@@ -174,7 +202,7 @@ class Script {
             let $td = $("<td>").appendTo($tr);
             $("<span>", { text: "0x" + mi.toString(16) }).appendTo($td);
             $td = $("<td>").appendTo($tr);
-            $("<span>", { text: easyrpgPlayer["HEAPU8"][mi] }).appendTo($td);
+            $("<span>", { text: easyrpgPlayer["HEAPU32"][mi] }).appendTo($td);
             $td = $("<td>").appendTo($tr);
             let $selbtn = $("<a>", { text: "选择", "data-index": mi, style: "cursor: pointer;" }).appendTo($td);
             $selbtn.click(() => {
@@ -188,34 +216,54 @@ class Script {
         }, 1000);
     }
 
+    getVariableIndex(variableNum) {
+        if (this.correctNum44Index <= 0) return -1;
+        let base = this.correctNum44Index - 44;
+        return base + variableNum;
+    }
+
     updateDataTable() {
         if (this.correctNum44Index <= 0) return;
         $("#rs-stat").text("当前状态");
+        $("#rs-addvar").show();
         let $mainTable = $("#rs-table");
         $mainTable.empty();
-        [
-            { title: "入梦次数", index: this.correctNum44Index - 4, length: 4, callFuc: HEAPU8Array2Int },
-            { title: "变量#44", index: this.correctNum44Index, length: 1, callFuc: null },
-            { title: "季节", index: this.correctNum44Index + 200, length: 1, callFuc: getSeasonString },
-        ].map((varLineData) => {
+        let dataTableData =
+            [
+                { title: "入梦次数", index: 43, callFuc: null },
+                { title: "变量#44", index: 44, callFuc: null },
+                { title: "季节", index: [43, 94], callFuc: getSeasonString },
+            ];
+        this.customVars.map((customData) => {
+            dataTableData.push(customData.toTableData());
+        });
+        dataTableData.map((varLineData, index) => {
             let $ltr = $("<tr>", { style: "width:100%;" });
             let $ltd = $("<td>", { style: "width:50%" }).appendTo($ltr);
-            $("<span>", { text: varLineData.title }).appendTo($ltd);
+            let $varTitle = $("<span>", { text: varLineData.title }).appendTo($ltd);
+            if (index > 2) { // 自定义项
+                $varTitle.click(() => {
+                    this.customVars = this.customVars.filter((customVarData) => customVarData.index !== varLineData.index);
+                    this.saveStorage();
+                    $varTitle.parent().parent().remove();
+                });
+            }
             $ltd = $("<td>").appendTo($ltr);
             let data;
-            if (varLineData.length > 1) {
+            if (varLineData.index instanceof Array === true) {
                 data = [];
-                for (let i = 0; i < varLineData.length; i++) data.push(easyrpgPlayer["HEAPU8"][varLineData.index + i]);
+                for (let i = 0; i < varLineData.index.length; i++) data.push(easyrpgPlayer["HEAPU32"][this.getVariableIndex(varLineData.index[i])]);
+                if (varLineData.callFuc) data = varLineData.callFuc(...data);
             }
             else {
-                data = easyrpgPlayer["HEAPU8"][varLineData.index];
+                data = easyrpgPlayer["HEAPU32"][this.getVariableIndex(varLineData.index)];
+                if (varLineData.callFuc) data = varLineData.callFuc(data);
             }
-            if (varLineData.callFuc) data = varLineData.callFuc(data);
             $("<span>", { text: data }).appendTo($ltd);
             $ltr.appendTo($mainTable);
         });
 
-        setTimeout(() => { this.updateDataTable(); }, 2000);
+        setTimeout(() => { this.updateDataTable(); }, 1000);
     }
 }
 
